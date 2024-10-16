bf995be4e9ca00107d7c0b417b172b2046442234
Vivek-3130 <vivekgzb31@gmail.com>
2024-10-16 21:48:54 +0530

    Upadated the AuthLayout and The Main.jsx file

--- blogWebApp/src/components/AuthLayout.jsx
+++ blogWebApp/src/components/AuthLayout.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { React, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-function Protected({children, authentication = true}) {
-  return (
+function Protected({ children, authentication = true }) {
+  const authStatus = useSelector((state) => state.auth.status);
+
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    // If logined nhi h
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
+
+  return loader ? (
+    <div className="loader-widget">
+      <div className="loader-spinner"></div>
+      <p>Loading...</p>
+    </div>
+  ) : (
     <>{children}</>
-  )
+  );
 }
 
-export default Protected
+export default Protected;
+
+// if(true){authentication required
+//     if(false){ authstatus is not yet true so mark it true
+//         navigator('/login')
+//     }
+// }
