98870fde2c42541466d224ddac090f3db1bad13d
Vivek-3130 <vivekgzb31@gmail.com>
2024-12-07 18:56:44 +0530

    Changes in The PostForm

---
+++ blogWebApp/src/components/post-form/PostForm.jsx
@@ -0,0 +1,74 @@
+import React, { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import Button from "../Button";
+import Input from "../Input";
+import RTE from "../RTE";
+import select from "../Select";
+import appwriteService from "../../appwrite/config";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function PostForm({ post }) {
+  const { register, handleSubmit, watch, setValue, getValues, control } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+ 
+  const navigate = useNavigate();
+
+  const userData = useSelector((state) => state.auth.userData);
+
+  const submit = async (data) => {};
+
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === "string")
+      return value
+        .trim()
+        .toLowerCase()
+        .replace(/[^a-zA-Z\d\s]+/g, "-")
+        .replace(/\s+/g, "-");
+  }, []);
+
+  React.useEffect(() => {
+    watch((value, { name }) => {
+      if (name === "title") {
+        setValue("slug", slugTransform(value.title), { shouldValidate: true });
+      }
+    });
+  }, [watch, slugTransform, setValue]);
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+      <div className="w-2/3 px-2">
+        <Input
+          label="Title"
+          placeholder="Title"
+          className="mb-4"
+          {...register("title", { required: true })}
+        />
+        <Input
+          label="Slug: "
+          placeholder="Slug"
+          className="mb-4"
+          {...register("slug", { required: true })}
+          onInput={(e) => {
+            setValue("slug", slugTransform(e.currentTarget.value), {
+              shouldValidate: true,
+            });
+          }}
+        />
+        <RTE
+          label="Content: "
+          name="content"
+          control={control}
+          defaultValue={getValues("content")}
+        />
+      </div>
+    </form>
+  );
+}
