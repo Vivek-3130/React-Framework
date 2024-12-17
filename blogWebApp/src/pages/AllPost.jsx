01da9c1d5397fb6df0807fc3d77d79ac7935c7f1
Vivek-3130 <vivekgzb31@gmail.com>
2024-12-17 18:49:41 +0530

    Components Updated

--- blogWebApp/src/pages/AllPost.jsx
+++ blogWebApp/src/pages/AllPost.jsx
@@ -3,6 +3,7 @@ import appwriteService from "../appwrite/config"
 import { useNavigate } from 'react-router-dom'
 import Container from '../components/container/Container'
 import PostCard from "../components/PostCard"
+
 function AllPost() {
   const [posts,setPosts]= useState(null)
 
@@ -14,9 +15,30 @@ function AllPost() {
         }
       })
     },[])
-  return (
-    <div className='py-6'>
 
+    if(posts.length === 0){
+      return (
+        <div className='w-full py-8'>
+        <Container>
+          <div className="flex flex-wrap">
+            <h1>Login to read posts</h1>
+          </div>
+        </Container>
+      </div> 
+      )
+    }
+    
+  return (
+    <div className='w-full py-8'>
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post}/>
+            </div>
+          ))}
+        </div>
+      </Container>
     </div>
   )
 }
